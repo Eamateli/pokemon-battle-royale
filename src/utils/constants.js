d681f824c1b5b7ee9ed6de299cd22f87b138ecc3
Eamateli <e.a.mateli@gmail.com>
2025-08-14 14:12:08 +0300

    Add context

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+// Action types for useReducer
+export const ACTIONS = {
+  SET_POKEMON: 'SET_POKEMON',
+  SET_LOADING: 'SET_LOADING',
+  SET_ERROR: 'SET_ERROR',
+  SET_VOTES: 'SET_VOTES',
+  SET_USER_VOTED: 'SET_USER_VOTED',
+  SET_CONNECTION_STATUS: 'SET_CONNECTION_STATUS',
+  RESET_BATTLE: 'RESET_BATTLE'
+};
+
+// Initial state for the battle reducer
+export const initialState = {
+  pokemon1: null,
+  pokemon2: null,
+  loading: true,
+  error: null,
+  votes: { pokemon1: 0, pokemon2: 0 },
+  userVoted: null,
+  connectionStatus: 'disconnected',
+  totalVotes: 0
+};
+
+// API endpoints and configuration
+export const API_CONFIG = {
+  POKEMON_BASE_URL: 'https://pokeapi.co/api/v2/pokemon/',
+  MAX_POKEMON_ID: 150, // Original 150 Pokémon
+  REQUEST_TIMEOUT: 10000 // 10 seconds
+};
